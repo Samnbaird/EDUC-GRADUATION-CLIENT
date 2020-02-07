b582ebfda755a9b48bbd0499e6303a89390b31eb
Michael Bonar <michael.w.bonar@gmail.com>
2020-02-07 05:51:28 -0800

    updated program rule & course achievement

--- src/components/CourseAchievement.vue
+++ src/components/CourseAchievement.vue
@@ -1,11 +1,14 @@
 <template>
   <router-link class="achievement-link" :to="{ name: 'achievement-show', params: { id: achievement.id} }">
     <div class="achievement-card -shadow">
-      <span class="eyebrow">Session: {{achievement.session}}</span>
-      <h4 class="student">{{achievement.student}}</h4>
-      <h5>Course: {{achievement.course}}</h5>
-      <h6>Grade: {{achievement.grade}}</h6>
-      <BaseIcon name="check-circle">{{ achievement.gradcodes.length }} graduation codes satisfied</BaseIcon>
+      <span class="eyebrow">ID: {{achievement.courseAchievementId}}</span>
+      <h4>Student: {{achievement.pen}}</h4>
+      <h5>Course: {{achievement.courseId}}</h5>
+      <h6>Final Letter Grade: {{achievement.finalLetterGrade}}</h6>
+      <h6>Final Percent: {{achievement.finalPercent}}</h6>
+      <h6>Interim Percent: {{achievement.interimPercent}}</h6>
+      <h6>Interim Letter Grade: {{achievement.interimLetterGrade}}</h6>
+      <h6>Course Type: {{achievement.courseType}}</h6> 
     </div>
   </router-link>
 </template>
