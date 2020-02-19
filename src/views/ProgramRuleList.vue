23e0f853b957bdee462a884ca7278891e3059d19
Shaun Lum <shaun.lum@gov.bc.ca>
2020-02-19 15:52:52 -0800

    Added Program Rules

--- src/views/ProgramRuleList.vue
+++ src/views/ProgramRuleList.vue
@@ -1,22 +1,63 @@
 <template>
-  <div>
-    <h2>List of Program Rules</h2>
-    <h6>To see program-rules you must run 'npx json-server --watch program-rules-db.json --port 3000'</h6>
-    <ProgramRule v-for="programrule in rules" :key="programrule.id" :programrule="rule" />
-    <BaseIcon />
+  
+  <div class="container">
+  <!-- Button trigger modal -->
+
+
+    <h1>Graduation Program Rules</h1>
+    <div class="search-input">
+      <input class="form-control" v-model="filters.name.value" placeholder="Enter a Rule"/>
+    </div>
+    <v-table
+        :data="rules"
+        :filters="filters"
+        :currentPage.sync="currentPage"
+        :pageSize="10"
+        @totalPagesChanged="totalPages = $event"
+        class="table table-responsive table-striped table-hover table-md text-center align-middle">
+          <thead slot="head">
+              <v-th sortKey="requirementCode">Requirement Code</v-th>
+              <v-th sortKey="requirementName">Requirement Name</v-th>
+              <v-th sortKey="requiredCredits">Requiremen Credits</v-th>
+              <v-th sortKey="notMetDescription">Not Met Description</v-th>
+              <v-th sortKey="programCode">Program Code</v-th>
+              <th><button type="button" class="add-course-achievement-button btn btn-primary" data-toggle="modal" data-target="#addCourseAchievementModal">Add Rule
+</button></th>
+          </thead>
+          <tbody slot="body" slot-scope="{displayData}">
+            <tr v-for="row in displayData" :key="row.requirementCode">
+              <td>{{row.requirementCode}}</td>
+              <td>{{row.requirementName}}</td>
+              <td>{{row.requiredCredits}}</td>
+              <td>{{row.notMetDescription}}</td>
+              <td>{{row.programCode}}</td>
+              <td><router-link class="achievement-link" :to="{ name: 'achievement-show', params: { id: row.id} }"><button class="btn btn-primary active">EDIT</button></router-link></td>
+            </tr>
+          </tbody>
+    </v-table>
+    <smart-pagination
+        :currentPage.sync="currentPage"
+        :totalPages="totalPages"
+      />
+
   </div>
 </template>
 
 <script>
-import ProgramRule from "@/components/ProgramRule";
 import ProgramRuleService from '@/services/ProgramRuleService.js';
 export default {
+  
   components: {
-    ProgramRule
   },
+  name: 'BasicFiltering',
   data() {
     return {
-      achievements: []
+      rules: [],
+      filters: {
+        name: { value: '', keys: ['requirementCode'] }
+      },
+      currentPage: 1,
+      totalPages: 0
     };
   },
   created() {
@@ -32,4 +73,20 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+#search{
+  display: none; /* will unhide when endpoint filter by pen is implemented */
+}
+.add-course-button{
+  float:right;
+}
+.search-input{
+  float:right;
+  width:300px;
+
+}
+.container h1{
+  width: 500px; 
+  float:left; 
+}
+</style>
